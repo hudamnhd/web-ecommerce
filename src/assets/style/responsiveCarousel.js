7eb207d97157649b29543b5a8c2e46c44bf41f28
hudamnhd <muhamadhuda519@gmail.com>
2023-06-20 14:14:50 +0700

    fix responsive carousel

--- src/assets/style/responsiveCarousel.js
+++ src/assets/style/responsiveCarousel.js
@@ -1,19 +1,27 @@
 export const responsive = {
-  superLargeDesktop: {
+  xl: {
     // the naming can be any, depends on you.
-    breakpoint: { max: 4000, min: 3000 },
-    items: 5,
-  },
-  desktop: {
-    breakpoint: { max: 3000, min: 1024 },
+    breakpoint: { max: 3000, min: 1200 },
     items: 6,
   },
-  tablet: {
-    breakpoint: { max: 1024, min: 464 },
+  lg: {
+    breakpoint: { max: 1200, min: 1024 },
+    items: 5,
+  },
+  md: {
+    breakpoint: { max: 1024, min: 768 },
+    items: 4,
+  },
+  sm: {
+    breakpoint: { max: 768, min: 620 },
+    items: 3,
+  },
+  ss: {
+    breakpoint: { max: 620, min: 480 },
     items: 2,
   },
-  mobile: {
-    breakpoint: { max: 464, min: 0 },
+  xs: {
+    breakpoint: { max: 480, min: 0 },
     items: 2,
   },
 };
